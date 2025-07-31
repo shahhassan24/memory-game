950fb1ddd059332000a8c71ef22baf33f59c68bb
shahdev <shah@circler.io>
2025-07-31 14:56:00 -0700

    sa

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,15 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      // Generate files in .svelte-kit/vercel/output
+      out: 'output',
+      precompress: true,
+    })
   }
 };
 
