865f7f478d7b486c59988d8fcba90d4cf5ac1e0a
shahdev <shahhassancs@gmail.com>
2026-03-11 17:27:59 +0500

    yo

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,12 @@
 import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
 
   kit: {
-    adapter: adapter({
-      runtime: 'nodejs20.x'
-    })
+    adapter: adapter()
   }
 };
 
